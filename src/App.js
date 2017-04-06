a2ed4b6c76ae33ea81de4d9e290cb23d6bab2a2f
kacampagnolli <kacampagnolli@gmail.com>
2017-04-05 22:53:40 -0300

    Login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.png';
 import './App.css';
+import { Divider, Form, Label , Container, Grid, Segment,Button,
+Image 
+} from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <Container>
+      <Grid columns='16'>
+         <Grid.Column>
+          </Grid.Column>
+      </Grid>
+         <Grid columns='16'>
+         <Grid.Column>
+          </Grid.Column>
+      </Grid>
+         <Grid columns='16'>
+         <Grid.Column>
+          </Grid.Column>
+      </Grid>
+    <Grid columns='16'>
+         <Grid.Column>
+          </Grid.Column>
+      </Grid>
+      <Grid columns='equal'>
+      <Grid.Column>
+    </Grid.Column>
+    <Grid.Column width={12}>
+      <Segment>
+ <center><Image src={logo} size='medium' shape='rounded' className='logo' fluid/></center>
+  <Form>
+    <Form.Field>
+      <input type='text' placeholder='User or Email' type='text' />
+    </Form.Field>
+    <Form.Field>
+      <input type='text' placeholder='Password' type='password' />
+    </Form.Field>
+     <Button color="gray" size='large' fluid >
+     Entrar
+  </Button>
+  </Form>
+  </Segment>
+  </Grid.Column>
+     <Grid.Column>
+    
+    </Grid.Column>
+
+  </Grid>
+  </Container>
+     
     );
   }
 }
+
+
 
 export default App;
